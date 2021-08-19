ce7685087f6eb7552f8e52288ce8f961e81fb3d1
Juan cadavid <jua1000n@gmail.com>
2021-08-19 10:25:45 -0500

    Entrega inicial

---
+++ Patterns/src/main/java/edu/eci/cvds/patterns/App.java
@@ -0,0 +1,18 @@
+package edu.eci.cvds.patterns;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println("");
+        System.out.println( "Hello World!");
+        for(String i: args){
+            System.out.println("hello"+i);
+        }
+        System.out.println("");
+    }
+}
